9854f08d017e0a20783aee7d019063a37adb45fe
LeandroNV <111306588+LeandroNV@users.noreply.github.com>
2024-01-25 19:45:26 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,67 @@
+# Encriptador y Desencriptador
+
+Hola! esta es mi solución al primer challenge propuesto en ONE (Oracle Next Education). La aplicación en cuestión te permite encriptar y desencriptar mensajes utilizando reglas específicas.
+
+## Reglas Predefinidas
+
+El Encriptador y Desencriptador utiliza las siguientes reglas predefinidas para encriptar y desencriptar mensajes:
+
+-   La letra "e" es convertida a "enter"
+-   La letra "i" es convertida a "imes"
+-   La letra "a" es convertida a "ai"
+-   La letra "o" es convertida a "ober"
+-   La letra "u" es convertida a "ufat"
+
+## Características
+
+-   Encripta mensajes según reglas predefinidas.
+-   Desencripta mensajes encriptados utilizando las mismas reglas.
+-   Interfaz de usuario amigable.
+-   Utiliza GSAP para animaciones suaves.
+-   Emite alertas con SweetAlert para notificaciones claras.
+
+## Tecnologías Utilizadas
+
+-   HTML5
+-   CSS3 (sin preprocesadores)
+-   Vanilla JavaScript
+-   GSAP (GreenSock Animation Platform)
+-   SweetAlert
+
+## Cómo Usar
+
+1. Ingresa un mensaje en el campo de texto.
+2. Haz clic en el botón "Encriptar" para encriptar el mensaje.
+3. Para desencriptar un mensaje encriptado, haz clic en el botón "Desencriptar".
+4. Se mostrará el mensaje encriptado o desencriptado en el área correspondiente.
+5. Se utilizarán animaciones suaves para mejorar la experiencia del usuario.
+6. Las alertas se emitirán con SweetAlert para notificar al usuario sobre el estado de la operación.
+
+## Demo
+
+Puedes encontrar una demostración en vivo de la aplicación [aquí](#).
+
+## Screenshots
+
+![Captura de Pantalla 1](screenshots/Preview%20Desktop.png)
+![Captura de Pantalla 2](screenshots/Preview%20Mobile.png)
+
+## Instalación
+
+No se requiere instalación. Simplemente abre el archivo HTML en tu navegador web para usar la aplicación.
+
+## Contribuir
+
+Si deseas contribuir a este proyecto, por favor sigue estas instrucciones:
+
+1. Haz un fork del repositorio.
+2. Clona tu fork: `git clone https://github.com/tu_usuario/encriptador.git`
+3. Crea una rama para tu nueva funcionalidad: `git checkout -b nueva-funcionalidad`
+4. Haz tus cambios y haz commit: `git commit -am 'Agregar nueva funcionalidad'`
+5. Empuja la rama a tu repositorio remoto: `git push origin nueva-funcionalidad`
+6. Haz un pull request en el repositorio original.
+
+---
+
+¡Gracias a los profesores [Christian Velasco](https://www.linkedin.com/in/christianpva/) y [Leonardo Castillo](https://www.linkedin.com/in/leonardo-castillo-4911571a/) por su excelente explicación a lo largo de todos los cursos!
+Por lo demás, cualquier [feedback](mailto:leonicvar@gmail.com) es bienvenido.
---
+++ js/app.js
@@ -0,0 +1,215 @@
+gsap.registerPlugin(TextPlugin);
+
+let outputText = document.getElementById("output");
+let btnCopy = document.getElementById("btnCopy");
+let titleOne = document.getElementById("title");
+let logo = document.getElementById("logo");
+let textInput = document.getElementById("input");
+let botones = document.getElementById("botones");
+let tituloOut = document.getElementById("titulo-output");
+let parrafoOut = document.getElementById("parrafo-output");
+let footerBy = document.getElementById("footer-by");
+
+/*============= ANIMACIONES GSAP ===============*/
+gsap.from("#title", {
+	duration: 1.2,
+	opacity: 0,
+	y: -50,
+	ease: "bounce",
+});
+
+gsap.from("#logo", {
+	duration: 2,
+	opacity: 0,
+	x: -50,
+	ease: "elastic.out",
+});
+
+gsap.from("#input", {
+	duration: 1,
+	opacity: 0,
+	x: -200,
+	ease: "back.out",
+});
+
+gsap.from("#botones", {
+	duration: 2,
+	opacity: 0,
+	y: 50,
+	ease: "elastic",
+});
+
+gsap.to(tituloOut, {
+	duration: 2,
+	text: "Ningún mensaje fue encontrado.",
+	ease: "none",
+});
+
+gsap.to(parrafoOut, {
+	duration: 2,
+	text: "Por favor, ingrese un mensaje para encriptar.",
+	ease: "none",
+});
+
+gsap.to(footerBy, {
+	duration: 2,
+	text: "© Code by Leandro Vargas",
+	ease: "none",
+});
+
+gsap.from("#footer-icons", {
+	duration: 2,
+	opacity: 0,
+	x: 50,
+	ease: "back.out",
+});
+
+/* =============================================== */
+
+/*============= FUNCIONES ===============*/
+
+function limpiarCaja() {
+	let input = document.getElementById("input");
+	let output = document.getElementById("output");
+
+	input.value = "";
+	output.textContent = "";
+}
+
+function animarTextoEncriptado(resultadoEncriptado) {
+	var outputText = document.getElementById("output");
+
+	// Animar el texto encriptado con GSAP
+	gsap.to(outputText, {
+		duration: 2,
+		text: resultadoEncriptado,
+		ease: "none",
+	});
+}
+
+function limitarTexto() {
+	let textarea = document.getElementById("input");
+	let contenido = textarea.value;
+
+	// Convertir todo el texto a minúsculas y sin acentos
+	let textoSinAcentos = contenido
+		.toLowerCase()
+		.normalize("NFD")
+		.replace(/[\u0300-\u036f]/g, "");
+
+	// Permitir solo letras y espacios
+	let textoFiltrado = textoSinAcentos.replace(/[^a-z ]/g, "");
+
+	// Actualizar el contenido del textarea
+	textarea.value = textoFiltrado;
+}
+
+function funcionAlerta(icon, title) {
+	const Toast = Swal.mixin({
+		toast: true,
+		position: "top-start",
+		showConfirmButton: false,
+		timer: 2000,
+		timerProgressBar: true,
+		didOpen: (toast) => {
+			toast.onmouseenter = Swal.stopTimer;
+			toast.onmouseleave = Swal.resumeTimer;
+		},
+	});
+	Toast.fire({
+		icon: icon,
+		title: title,
+	});
+}
+
+function addClass(elemento = String, clase = String) {
+	let elementoHTML = document.getElementById(elemento);
+	elementoHTML.classList.add(clase);
+	return;
+}
+
+function removeClass(elemento = String, clase = String) {
+	let elementoHTML = document.getElementById(elemento);
+	elementoHTML.classList.remove(clase);
+	return;
+}
+
+function asignarTextoElemento(elemento = String, texto = String) {
+	let elementoHTML = document.getElementById(elemento);
+	elementoHTML.innerHTML = texto;
+	return;
+}
+
+function encriptar() {
+	let input = document.getElementById("input");
+	let output = input.value;
+
+	// Reemplazar letras según las reglas
+	let resultadoEncriptado = output
+		.replace(/e/g, "enter")
+		.replace(/i/g, "imes")
+		.replace(/a/g, "ai")
+		.replace(/o/g, "ober")
+		.replace(/u/g, "ufat");
+
+	// Agregar clase para mostrar el texto encriptado
+	addClass("outputIni", "remove");
+	// Remover clase para mostrar el botón de copiar
+	removeClass("btnCopy", "remove");
+
+	// Animar el texto encriptado con GSAP
+	animarTextoEncriptado(resultadoEncriptado);
+	// Cambiar el color del botón de copiar
+	btnCopy.style.backgroundColor = "#ffffff";
+	// Cambiar el color de la letra del botón de copiar
+	btnCopy.style.color = "#0a3871";
+	btnCopy.style.border = "1px solid #0a3871";
+
+	// Cambiar el texto del botón de copiar
+	btnCopy.textContent = "Copiar";
+	// Mostrar alerta
+	funcionAlerta("success", "El texto se ha encriptado correctamente");
+	// Habilitar el botón de copiar
+	btnCopy.removeAttribute("disabled");
+}
+
+function desencriptar() {
+	let input = document.getElementById("input");
+	let output = input.value;
+
+	// Reemplazar letras según las reglas
+	let resultadoDesencriptado = output
+		.replace(/enter/g, "e")
+		.replace(/imes/g, "i")
+		.replace(/ai/g, "a")
+		.replace(/ober/g, "o")
+		.replace(/ufat/g, "u");
+
+	//Agregar clase para mostrar el texto desencriptado
+	addClass("outputIni", "remove");
+	// Mostrar el texto encriptado
+	animarTextoEncriptado(resultadoDesencriptado);
+	// Cambiar el color del botón de copiar
+	btnCopy.style.backgroundColor = "#ffffff";
+	// Cambiar el color de la letra del botón de copiar
+	btnCopy.style.color = "#0a3871";
+	btnCopy.style.border = "1px solid #0a3871";
+
+	// Cambiar el texto del botón de copiar
+	btnCopy.textContent = "Copiar";
+	// Mostrar alerta
+	funcionAlerta("success", "El texto se ha desencriptado correctamente");
+	// Habilitar el botón de copiar
+	btnCopy.removeAttribute("disabled");
+}
+
+function copiar() {
+	navigator.clipboard.writeText(outputText.textContent);
+	btnCopy.style.backgroundColor = "#2ecc71";
+	btnCopy.style.color = "#ffffff";
+	btnCopy.style.border = "none";
+	btnCopy.textContent = "Copiado!";
+	btnCopy.setAttribute("disabled", true);
+	limpiarCaja();
+	funcionAlerta("success", "Texto copiado al portapapeles");
+}
