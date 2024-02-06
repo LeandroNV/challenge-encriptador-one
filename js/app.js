c186c72d841db7d29b47b8988be9f477a91bb029
LeandroNV <111306588+LeandroNV@users.noreply.github.com>
2024-02-06 16:15:25 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,43 +18,139 @@ let isDeleting = false;
 let isPaused = false;
 let pauseEnd = 0;
 
-/*============= ANIMACIONES GSAP ===============*/
-// gsap.from("#title", {
-// 	duration: 1.5,
-// 	opacity: 0,
-// 	x: -50,
-// 	ease: "elastic.out",
-// });
+/* ============ ANIMACIÓN LOADER =============== */
+function removeLoaderAfterDelay(delayTime) {
+	setTimeout(() => {
+		const loaderElement = document.querySelector(".loader-container");
+		if (loaderElement) {
+			loaderElement.remove(); // Elimina el loader del DOM
+		}
+	}, delayTime);
+}
 
-gsap.from("#logo", {
-	duration: 1.5,
+function startLoader() {
+	let counterElement = document.querySelector(".count p");
+	let currentValue = 0;
+
+	function updateCounter() {
+		if (currentValue < 100) {
+			let increment = Math.floor(Math.random() * 10) + 1;
+			currentValue = Math.min(currentValue + increment, 100);
+			counterElement.textContent = currentValue;
+
+			let delay = Math.floor(Math.random() * 200) + 25;
+			setTimeout(updateCounter, delay);
+		}
+	}
+
+	updateCounter();
+}
+
+startLoader(); // Iniciar el loader
+
+// Llamar a la función removeLoaderAfterDelay() para eliminar el loader después de un tiempo específico
+removeLoaderAfterDelay(6000);
+
+gsap.to(".count", { opacity: 0, delay: 3.5, duration: 0.25 });
+
+let textWrapper = document.querySelector(".ml16");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+	/\S/g,
+	"<span class='letter'>$&</span>"
+);
+
+anime
+	.timeline({ loop: false })
+	.add({
+		targets: ".ml16 .letter",
+		translateY: [-100, 0],
+		easing: "easeOutExpo",
+		duration: 1500,
+		delay: (el, i) => 30 * i,
+	})
+	.add({
+		targets: ".ml16 .letter",
+		translateY: [0, 100],
+		easing: "easeOutExpo",
+		duration: 3000,
+		delay: (el, i) => 2000 + 30 * i,
+	});
+
+gsap.to(".count", {
 	opacity: 0,
-	x: -90,
-	ease: "elastic.out",
+	ease: "power2.inOut",
+	duration: 0.5,
+	delay: 3.75,
+});
+
+gsap.to(".pre-loader", {
+	scale: 0.5,
+	ease: "power4.inOut",
+	duration: 2,
+	delay: 3,
+});
+
+gsap.to(".loader", {
+	height: "0",
+	ease: "power4.inOut",
+	duration: 1.5,
+	delay: 3.75,
+});
+
+gsap.to(".loader-bg", {
+	height: "0",
+	ease: "power4.inOut",
+	duration: 1.5,
+	delay: 4,
+	onComplete: function () {
+		// Cambiar el z-index de .site-content para que esté por encima del loader
+		document.querySelector(".site-content").style.zIndex = "1";
+	},
+});
+
+gsap.to(".loader-2", {
+	clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+	ease: "power4.inOut",
+	duration: 1.5,
+	delay: 3.5,
+});
+
+/*============= ANIMACIONES GSAP ===============*/
+
+gsap.from(".site-content", {
+	y: 200,
+	ease: "power4.inOut",
+	duration: 1.5,
+	delay: 3.5,
+	stagger: 0.05,
 });
 
 gsap.to(tituloOut, {
 	duration: 1.5,
 	text: "Ningún mensaje fue encontrado.",
 	ease: "none",
+	delay: 4.5,
 });
 
 gsap.to(titleColor, {
 	duration: 1.5,
 	text: "ENCRYPTOR",
 	ease: "none",
+	delay: 4.5,
 });
 
 gsap.to(parrafoOut, {
 	duration: 1.5,
 	text: "Por favor, ingrese un mensaje para encriptar.",
 	ease: "none",
+	delay: 4.5,
 });
 
 gsap.to(footerBy, {
 	duration: 1.5,
 	text: "© Code by Leandro Vargas",
 	ease: "none",
+	delay: 4.5,
 });
 
 gsap.from("#footer-icons", {
@@ -62,6 +158,23 @@ gsap.from("#footer-icons", {
 	opacity: 0,
 	x: 50,
 	ease: "back.out",
+	delay: 4.5,
+});
+
+gsap.to("#logo", {
+	clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+	ease: "power4.inOut",
+	duration: 1.5,
+	delay: 4,
+	stagger: 0.25,
+});
+
+gsap.to(".figure", {
+	clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+	ease: "power4.inOut",
+	duration: 1.5,
+	delay: 4.5,
+	stagger: 0.25,
 });
 
 function animar(id = String, ease = String) {
@@ -203,11 +316,11 @@ function desencriptar() {
 
 	// Reemplazar letras según las reglas
 	const resultadoDesencriptado = output
-		.replace(/ufat/g, "u")
-		.replace(/ober/g, "o")
-		.replace(/ai/g, "a")
+		.replace(/enter/g, "e")
 		.replace(/imes/g, "i")
-		.replace(/enter/g, "e");
+		.replace(/ai/g, "a")
+		.replace(/ober/g, "o")
+		.replace(/ufat/g, "u");
 
 	//Agregar clase para mostrar el texto desencriptado
 	addClass("outputIni", "remove");
@@ -218,10 +331,6 @@ function desencriptar() {
 	// Cambiar el color de la letra del botón de copiar
 	btnCopy.style.color = "#0a3871";
 	btnCopy.style.border = "1px solid #0a3871";
-
-	// Cambiar el texto del botón de copiar
-	// btnCopy.textContent = "Copiar";
-	// Mostrar alerta
 	funcionAlerta("success", "El texto se ha desencriptado correctamente");
 	// Mostrar el output
 	removeClass("OutputFin", "remove");
@@ -231,10 +340,6 @@ function desencriptar() {
 
 function copiar() {
 	navigator.clipboard.writeText(outputText.textContent);
-	// btnCopy.style.backgroundColor = "#2ecc71";
-	// btnCopy.style.color = "#ffffff";
-	// btnCopy.style.border = "none";
-	// btnCopy.textContent = "Copiado!";
 	btnCopy.setAttribute("disabled", true);
 	limpiarCaja();
 	addClass("OutputFin", "remove");
